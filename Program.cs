eba693531c8fa05ce47bf38ffd8e76bff0b9e635
hayant <antti.hayrinen@icloud.com>
2025-10-09 19:44:59 +0300

    Hello World!

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//// app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();
+// app.UseStaticFiles(new StaticFileOptions
+// {
+//     FileProvider = new PhysicalFileProvider(
+//         Path.Combine(Directory.GetCurrentDirectory(), "scripts/dist")),
+//     RequestPath = string.Empty
+// });
+app.UseStaticFiles();
+
+//app.MapFallbackToFile("index.html");
 
 app.Run();
---
+++ scripts/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../wwwroot"),
+    clean: true,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    port: 3000,
+    open: true,
+  },
+};
